887c28f4a68131fd4ddfcc5be5343c94aeab6737
lucas874 <lsc500@outlook.dk>
2025-08-26 17:47:46 +0200

    Cli experiments

--- generate-event-spec/src/actyxToProtobuf.ts
+++ generate-event-spec/src/actyxToProtobuf.ts
@@ -3,12 +3,11 @@ import { generateProtoBufMsgDefs } from "./protobuf_codegen.js";
 import { eventSpecToProtoBuf } from "./encode_protobuf.js";
 import { extractTypesFromFileCleaned } from "./extract_types.js";
 import path from "path";
-//export const ax2pb = new Command("ax2pb");
 
 type Options = {
-    outputfile: string,
-    package: string,
-    isBranchTracking: boolean
+    output: string,
+    packageName: string,
+    branchTracking: boolean
 }
 
 export const ax2pb = new Command("ax2pb")
@@ -17,6 +16,4 @@ export const ax2pb = new Command("ax2pb")
     .option("-o, --output <FILE>", "Output file (defaults to output.proto)", "output.proto")
     .option("-p, --package-name <PACKAGE>", "Name to give package containing message types (defaults to ;myPackage').", "myPackage")
     .option("-b, --branch-tracking", "Include last updating event field in message types.", false)
-    .action(async (file: string, options: Options) => { generateProtoBufMsgDefs(eventSpecToProtoBuf(options.package, extractTypesFromFileCleaned(path.resolve(file)), options.isBranchTracking), options.outputfile)})
-//ax2pb.exe
-    //console.log(file), console.log(typeof(file)), console.log(process.cwd()), console.log(path.resolve(process.cwd(), file))
+    .action((file: string, options: Options) =>  generateProtoBufMsgDefs(eventSpecToProtoBuf(options.packageName, extractTypesFromFileCleaned(path.resolve(process.cwd(), file)), options.branchTracking), path.resolve(process.cwd(), options.output)))
